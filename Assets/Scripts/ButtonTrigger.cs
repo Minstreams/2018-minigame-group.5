6edfc58cf0fd58f83aac94b78b9873db5e2a7b39
Minstreams <minstreams@qq.com>
2018-11-16 15:19:33 +0800

    目录整理

---
+++ Assets/Scripts/ButtonTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[AddComponentMenu("自制工具/Button Trigger")]
+public class ButtonTrigger : MonoBehaviour
+{
+    private Button button;
+    public string message;
+    private void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(() => { Debug.Log(message); });
+    }
+}
