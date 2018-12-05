69b8c6a627517220d1ca7669a81109c9d3524a61
4057492 <837233905@qq.com>
2018-12-05 16:27:03 +0800

    land

---
+++ Assets/GroundResource/TestPM.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TestPM : MonoBehaviour {
+
+	private Rigidbody rig;
+
+	private Vector3 oriPos;
+
+	public Vector3 force = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		rig = GetComponent <Rigidbody> ();
+		oriPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void AddForce(){
+		rig.AddForce (force);
+	}
+
+	public void Replace(){
+		transform.position = oriPos;
+	}
+}
+
+
